d676349ecd4aac895585f12fbef913cbf4925132
hermeneutika <hermeneutika@msn.com>
2022-05-01 19:15:11 +0100

    1/5/22 1915

---
+++ chokmah.online/public_html/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Wisdom is the principal thing</title>
+  <link rel="stylesheet" href="css/menu.css" />
+  <link rel="stylesheet" href="css/header.css" />
+  <link rel="stylesheet" href="css/site.css" />
+</head>
+<body>
+<?php
+
+include("counter.php");
+echo str_repeat('&nbsp;', 50); // adds 5 spaces;
+?>
+  
+  <?php include ("menu1.php"); ?>  
+</body>
+</html>
---
+++ chokmah.online/public_html/menu1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<body>
+<div class="container">
+  <h3>Wisdom is the principal thing</h3>
+
+  <nav>
+    <!--<ul class="nav-menu nav-center"> -->
+    <!--<ul class="nav-menu nav-vertical"> -->
+      <ul class="nav-menu nav-center">
+      <li>
+  
+    <a href="#">Chokmah and Binah</a> 
+    <ul>
+    <li><a href="alanon1.php">Chokmah</a></li>    
+</ul>
+</li>
+
+
+</nav>
+</body>
+</html>
