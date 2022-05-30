dacd93c01db744e7e964f2af02ccb448f5e47a9c
hermeneutika <hermeneutika@msn.com>
2022-05-30 09:01:49 +0100

    30/5/22 0901

--- hermeneutika.uk/public_html/history1.php
+++ hermeneutika.uk/public_html/history1.php
@@ -1,16 +1,88 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="menu.css" />
-  <link rel="stylesheet" href="header.css" />
-  <link rel="stylesheet" href="site.css" />
-  <title>Document</title>
-</head>
-<body>
-<?php include ("menu1.php"); ?> 
-  
-</body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+  <head>
+    <meta http-equiv="CONTENT-TYPE" content="text/html; charset=windows-1252" />
+    <title></title>
+    <meta name="GENERATOR" content="OpenOffice 4.1.11  (Win32)" />
+    <meta name="CREATED" content="20220507;11210930" />
+    <meta name="CHANGED" content="20220516;10180084" />
+    <style type="text/css">
+      <!--
+      	@page { margin: 2cm }
+      	P { margin-bottom: 0.21cm }
+      	P.western { so-language: en-GB }
+      -->
+    </style>
+    <link rel="stylesheet" href="menu.css" />
+    <link rel="stylesheet" href="header.css" />
+    <link rel="stylesheet" href="site.css" />
+  </head>
+  <body lang="en-GB" dir="LTR">
+		<?php include ("menu1.php"); ?>
+    <li><a href="daniel-links.php">List of various people involved</a></li>
+    <p class="western" style="margin-bottom: 0cm">
+      <font size="5">HISTORY OF THE BIBLE AND ITS INTERPRETATION</font>
+    </p>
+    <p class="western" style="margin-bottom: 0cm"><br /></p>
+    <p class="western" style="margin-bottom: 0cm">
+      <font size="5"
+        >After many years of looking into the Bible and also attending various
+        &ldquo;Christian&rdquo; institutions and listening to a lot of various
+        &ldquo;Christian teachers,prophets etc, and reading a lot of books on
+        the subject. Of course each prophet and teacher thinks or even demands
+        that they alone have the correct &ldquo;angle&rdquo; on the entire
+        subject. Criticism is just not allowed, the status quo must be
+        maintained at all costs. The party line must be held. I for one need the
+        freedom to disagree with the ruling majority. In other words the ability
+        to dissent from the majority view. In countries like China,or North
+        Korea or Russia of course dissent is forbidden. And even in the so
+        called democratic liberal West, it becomes harder to challenge the
+        status quo. But thankfully so far, not impossible. But even in the West
+        it is hard to say challenge climate change,or same sex marriage, or so
+        called gender issues. One is expected to conform it would seem.
+      </font>
+    </p>
+    <p class="western" style="margin-bottom: 0cm"><br /></p>
+    <p class="western" style="margin-bottom: 0cm">
+      <font size="5"
+        >The same is true of our religious institutions it would seem. It is
+        verboten to challenge the prevailing orthodoxy. So what is going on? For
+        decades I have been saying &ldquo;faulty diagnosis, faulty
+        solution&rdquo;. What happens if my diagnosis of the Christian issue has
+        been incorrect? What happens if say Reformed Protestant fundamentalism
+        is actually not true? The Pentecostals have had it wrong for decades.
+        And then to actually challenge my own reformed &ldquo;theology&rdquo; is
+        a very difficult thing to do. Sola Scriptura is not then the truth of
+        the matter. And then perhaps the Roman Catholic position is then
+        correct? The Magisterium then becomes a valid source of data? Then ones
+        view of the Sacred Texts and their interpretations becomes a matter of
+        critical moment. And this then becomes a power relationship. One must
+        accept the prevailing orthodoxy upon pain of death, or worse,exclusion.
+        The Guardians of Orthodoxy have powerful weapons at their
+        disposal.</font
+      >
+    </p>
+    <p class="western" style="margin-bottom: 0cm">
+      <font size="5"
+        >But there have been individuals in the past who have challenged
+        orthodoxy. Not out of a sense of rebellion but as a question of Truth.
+      </font>
+    </p>
+    <p class="western" style="margin-bottom: 0cm">
+      <font size="5"
+        >So my researches into the history of the Bible led me to find out that
+        even in the Christian Church a lot of people believe that the book of
+        the prophet Daniel was written late. Thus,in my view casting aspersions
+        on the Word of God. If one is to accept a late dating of the book of the
+        prophet Daniel, then one is casting doubt onto the veracity of the Word
+        of God.
+      </font>
+    </p>
+    <p class="western" style="margin-bottom: 0cm">
+      <font size="5"
+        >So from a simple desire to understand the history of the Bible, to
+        getting involved on a major controversy.
+      </font>
+    </p>
+  </body>
 </html>
