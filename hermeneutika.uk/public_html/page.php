6e93375551f3bbc5dcf3305698479bec974e67bb
hermeneutika <hermeneutika@msn.com>
2022-06-26 13:23:39 +0100

    26/6/22 1323

--- hermeneutika.uk/public_html/page.php
+++ hermeneutika.uk/public_html/page.php
@@ -7,27 +7,34 @@ $bb = $_SESSION['bible'];
 $comment=$_SESSION['comment'];
 echo "session var comment".$comment;
 echo "variable bb=".$bb;
-echo "mytextarea".$_POST['myTextarea'];
+echo "mytextarea from the post".$_POST['myTextarea'];
+$mytextarea=$_POST['myTextarea'];
+echo "mytextarea=".$mytextarea;
 #echo  $_POST['$bb'];
  include ("conn.php"); 
  #include ("menu1.php"); 
  echo "print before the sql"; 
- $sql1="select text from michael where full='1001002'";
+ $sql1="select text from michael where full =$bb";
 $result = $conn->query($sql1);
 $row = mysqli_fetch_array($result, MYSQLI_BOTH);
 #if ($result->num_rows > 0) {
- # // output data of each row
+  // output data of each row
   #while($row = $result->fetch_assoc()) {
     echo "id: " . $row["text"] ;
     $var1=$row["text"];
-    echo "variable=".$var1;
+    echo "variable var1 =".$var1;
   
 
+$sql3="UPDATE michael SET text=CONCAT('$var1',' ','$mytextarea') where full=$bb";
+if ($conn->query($sql3) === TRUE) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . $conn->error;
+}
 
-
-$sql="update michael set text=CONCAT(?,?) WHERE FULL='1001005'";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("ss",$var1,$contents);
-$stmt->execute();
+#$sql="UPDATE michael SET text=CONCAT(?,?) WHERE FULL='1001001'";
+#$stmt = $conn->prepare($sql);
+#$stmt->bind_param("ss",$var1,$contents);
+#$stmt->execute();
 
 ?>
